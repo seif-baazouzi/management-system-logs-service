d20b695905aee459b1abefc3cb5a811cb40a8d28
seif <baazouziseif9@gmail.com>
2022-10-05 11:37:07 +0100

    init server and create database

---
+++ database/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE logs;
+
+\c logs
+
+CREATE TABLE logs (
+    logID UUID PRIMARY KEY,
+    label VARCHAR NOT NULL, 
+    description VARCHAR NOT NULL, 
+    value INT NOT NULL, 
+    userID UUID NOT NULL,
+    workspaceID UUID NOT NULL,
+    createdAt TIMESTAMP DEFAULT NOW() 
+);
+
+CREATE INDEX UserIdInex ON logs USING hash (userID);
+CREATE INDEX WorkspaceInex ON logs USING hash (workspaceID);
